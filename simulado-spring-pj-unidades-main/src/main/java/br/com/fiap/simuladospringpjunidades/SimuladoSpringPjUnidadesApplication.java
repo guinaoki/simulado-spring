f0c5f795021d19f5aaccaf5e5ea0c741ef2e172c
Guilherme Naoki Hirayama <126685457+guinaoki@users.noreply.github.com>
2024-04-17 10:43:50 -0300

    Add files via upload

---
+++ simulado-spring-pj-unidades-main/src/main/java/br/com/fiap/simuladospringpjunidades/SimuladoSpringPjUnidadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.simuladospringpjunidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladoSpringPjUnidadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( SimuladoSpringPjUnidadesApplication.class, args );
+    }
+
+}
---
+++ simulado-spring-pj-unidades-main/src/test/java/br/com/fiap/simuladospringpjunidades/SimuladoSpringPjUnidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.simuladospringpjunidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladoSpringPjUnidadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
